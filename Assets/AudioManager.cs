505565c5e06322787f1ee5ac6d9b408573e66a4d
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2021-04-29 09:26:46 +0200

    Music

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -3,33 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+[Serializable]
+public struct Clip
+{
+    public AudioClip playerTurnClip;
+    public AudioClip IATurnClip;
+}
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance;
+    public float transitionWait = 1;
+    public Clip[] musicClips;
 
-    public AudioClip[] music;
-    private void Awake()
+    private AudioSource music;
+    public AudioSource transition;
+    private bool playerTurn = false;
+    private TurnManager turn;
+    private float prevVolume;
+    private bool transitioning;
+    private void Start()
     {
-        if (Instance == null)
+        music = GetComponent<AudioSource>();
+        music.loop = true;
+
+        turn = FindObjectOfType<TurnManager>();
+    }
+
+    private void Update()
+    {
+        if (turn)
         {
-            Instance = this;
+            if (!playerTurn && turn.PlayerTurn || playerTurn && !turn.PlayerTurn)
+            {
+                transitioning = true;
+                StartCoroutine(ChangeTurn());
+            }
+        }
+        if (music.volume == 0 && !transitioning)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FadeIn(transitionWait, prevVolume));
+        }
+    }
+
+    private IEnumerator ChangeTurn()
+    {
+        playerTurn = !playerTurn;
+        //transition.Play();
+        StartCoroutine(FadeOut(transitionWait));
+        prevVolume = music.volume;
+
+        yield return new WaitForSeconds(transitionWait);
+
+        if (playerTurn)
+        {
+            music.clip = musicClips[0].playerTurnClip;
         }
         else
         {
-            Destroy(gameObject);
+            music.clip = musicClips[0].IATurnClip;
         }
+        music.Play();
+        StartCoroutine(FadeIn(transitionWait, prevVolume));
+        transitioning = false;
     }
-
-    private void Start()
+    private IEnumerator FadeIn(float duration, float targetVolume)
     {
-        
-    }
+        float currentTime = 0;
+        float start = 0;
 
-    private void OnChangeTurn()
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            music.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+
+            if (start != prevVolume) //someone change the settings transitioning
+                prevVolume = start;
+            yield return null;
+        }
+        yield break;
+    }
+    private IEnumerator FadeOut(float duration)
     {
-
+        float start = music.volume;
+        while (music.volume > 0)
+        {
+            music.volume -= start * Time.deltaTime / duration;
+            if (start != prevVolume) //someone change the settings transitioning
+                prevVolume = start;
+            yield return null;
+        }
+        yield break;
     }
-
     private void ChageVolume(float value)
     {
         
